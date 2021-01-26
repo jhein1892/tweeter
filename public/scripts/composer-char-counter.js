b4453185a739302cbca7d6ad9f898ec077c2e84f
Jacob Hein <jacobhein1892@gmail.com>
2021-01-26 20:29:37 +0000

    added tweet box

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,15 +1,15 @@
-
-
-// const callback = function() {
-//   alert('Jello');
-// };
-
-// el.addEventListener('dblclick', callback);
-
 $(document).ready(() => {
   let num = $(".counter").val();
   $("#tweet-text").keyup(() => {
     let string = $("#tweet-text").val()
     $(".counter").val(num - string.length)
+    let count = num - string.length;
+    if (count <= 0){
+      $('.counter').css("color", "red")
+    } else if (count <= 50 && count > 0) {
+      $('.counter').css("color", "#fba607")
+    } else {
+      $('.counter').css("color", "#000645")
+    }
   })
-})
+}) 
