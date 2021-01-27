82f0fd724ae5cf6ee1a29a53f915c9af2828d3a8
Jacob Hein <jacobhein1892@gmail.com>
2021-01-27 03:15:28 +0000

    ajax get on /tweets so that we can show all the previous tweets on the home page

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,39 +3,6 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-const tweetData = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1611523884350
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd"
-    },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1611610284350
-  }
-]
-
-const renderTweets = function (tweets) {
-  let output = []
-  for (let tweet in tweets){
-    output.push(createTweetElement(tweets[tweet]));
-  };
-return output; 
-}
-
 const createTweetElement = function (data) {
   const markup =
     `<article>
@@ -57,20 +24,35 @@ const createTweetElement = function (data) {
     return markup; 
 }
 
-const $tweet = renderTweets(tweetData)
+
 $(document).ready( () => {
-  $('#container').append($tweet);
+
+  const renderTweets = function (tweets) {
+    let output = []
+    for (let tweet in tweets){
+      output.push(createTweetElement(tweets[tweet]));
+    };
+  return output; 
+  }
 
   $(".submit-tweet").submit(function(event) { 
     event.preventDefault();
     const $queryString = $(this).serialize();
-    console.log($queryString);
     $.post("/tweets", $queryString, function(response) {
       console.log('Working')
     })
-    // $.ajax('/tweets', queryString,{method: 'POST'})
-    // })
   });
+  
+  const loadtweets = function(){
+    $.get("/tweets", 'string', function(response) {
+       let myTweets = renderTweets(response); 
+    $.get('/tweets', myTweets, function() {
+      console.log("Will it work?")
+      $('#container').append(myTweets)
+    })
+  })
+}
+loadtweets()
 })
 
 
