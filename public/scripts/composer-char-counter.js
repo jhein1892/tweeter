af1573b409b562f522007455361203fed422d0f7
Jacob Hein <jacobhein1892@gmail.com>
2021-01-28 23:36:58 +0000

    fixed quotation inconsistencies in composer file

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,17 +1,17 @@
 $(document).ready(() => {
-  let num = $(".counter").val();
-  $(".submit-tweet").on('keyup',function() {
+  let num = $('.counter').val();
+  $('.submit-tweet').on('keyup',function() {
     let counter = $(this.children[1].children[1])
     let string = $(this.children[0]).val()
     let count = num - string.length; 
     counter.val(count) 
  
     if (count <= 0){
-      counter.css("color", "red");
+      counter.css('color', 'red');
     } else if (count <= 50 && count > 0) {
-      counter.css("color", "#fba607")
+      counter.css('color', '#fba607')
     } else {
-      counter.css("color", "#000645")
+      counter.css('color', '#000645')
     }
   })
   
